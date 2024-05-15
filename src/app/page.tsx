8fea985694750aaac1e3cc3b81d0f1965fb37835
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2024-05-15 02:12:16 -0400

    Quill init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-500 font-semibold ">
+      <div>Shehryar is King</div>
+    </MaxWidthWrapper>
+  );
+}
