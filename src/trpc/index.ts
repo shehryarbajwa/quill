5af593fc8cce3039ccea1419ca7e3a5c96126b10
Shehryar <shehryar.bajwa@gmail.com>
2024-05-24 18:43:56 -0400

    setup schema and postgres

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from "@/db";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -9,6 +10,21 @@ export const appRouter = router({
 
     if (!user || !user.id || !user.email)
       throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      }
+    })
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      })
+    }
 
     return { success: true }
   }),
