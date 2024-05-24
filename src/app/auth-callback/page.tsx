0bf046dbdb459e1f67bb7e16397cd42328326275
Shehryar <shehryar.bajwa@gmail.com>
2024-05-24 17:54:06 -0400

    added auth callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,37 +1,48 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
+import React, { Suspense } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { Loader2 } from 'lucide-react';
 import { trpc } from '../_trpc/client';
-import { useEffect } from 'react';
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
-  console.log('this is origin', origin);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await trpc.authCallback.useQuery(undefined);
-        if (data?.success) {
-          router.push(origin ? `/${origin}` : '/dashboard');
-        }
-      } catch (err: any) {
-        if (err.data?.code === 'UNAUTHORIZED') {
-          router.push('/sign-in');
-        }
-      }
-    };
+  const { data, error } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
-    fetchData();
+  if (error) {
+    if (error.data?.code === 'UNAUTHORIZED') {
+      router.push('/sign-in');
+    }
+    return null;
+  }
 
-    const intervalId = setInterval(fetchData, 500); // Retry every 500ms
+  if (data && data.success) {
+    router.push(origin ? `/${origin}` : '/dashboard');
+  }
 
-    return () => clearInterval(intervalId);
-  }, [router, origin]);
-
-  return null; // You can return your UI here if needed
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
-export default Page;
+const SuspensePage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page />
+    </Suspense>
+  );
+};
+
+export default SuspensePage;
