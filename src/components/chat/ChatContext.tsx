1687e852495e41bdc95fed8cc6e00ce9982248cf
Shehryar <shehryar.bajwa@gmail.com>
2024-07-26 21:04:47 -0400

    create optimistic loading for chat

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useRef, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { trpc } from '@/app/_trpc/client';
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+import { toast } from '../ui/use-toast';
 
 type StreamResponse = {
   addMessage: () => void;
@@ -46,7 +47,7 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
 
       return response.body;
     },
-    onMutate: async () => {
+    onMutate: async ({ message }) => {
       (backupMessage.current = message), setMessage('');
 
       // Step 1
@@ -87,6 +88,114 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
           };
         }
       );
+
+      setIsLoading(true);
+
+      return {
+        previousMessages:
+          previousMessages?.pages.flatMap((page) => page.messages) ?? [],
+      };
+    },
+    onSuccess: async (stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description: 'Please refresh this page and try again',
+          variant: 'destructive',
+        });
+      }
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+
+      let done = false;
+
+      // accumulated response
+      let accResponse = '';
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+
+        const chunkValue = decoder.decode(value);
+
+        accResponse += chunkValue;
+
+        // append chunk to the actual message
+
+        utils.getFileMessages.setInfiniteData(
+          {
+            fileId,
+            limit: INFINITE_QUERY_LIMIT,
+          },
+          (old) => {
+            if (!old)
+              return {
+                pages: [],
+                pageParams: [],
+              };
+
+            let isAiResponseCreated = old.pages.some((page) =>
+              page.messages.some((message) => message.id === 'ai-response')
+            );
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages;
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: 'ai-response',
+                      text: accResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ];
+                } else {
+                  updatedMessages = page.messages.map((message) => {
+                    if (message.id === 'ai-response') {
+                      return {
+                        ...message,
+                        text: accResponse,
+                      };
+                    }
+                    return message;
+                  });
+                }
+                //For each page in old.pages, we are overriding the messages property with the updateMessages
+
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                };
+              }
+              //If it's not the first page, where page === old.pages[0]
+              //Then we return the original page without any modifications
+
+              return page;
+            });
+
+            //Returning the old data overriding pages with updated Pages
+
+            return { ...old, pages: updatedPages };
+          }
+        );
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current);
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessages ?? [] }
+      );
+    },
+    onSettled: async () => {
+      setIsLoading(false);
+      await utils.getFileMessages.invalidate({ fileId });
     },
   });
 
