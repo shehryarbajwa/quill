e7a4a72fba11cf44f493ba50778742c4871a93d2
Shehryar <shehryar.bajwa@gmail.com>
2024-08-19 02:39:09 -0400

    updated promise for kindeAuth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest } from 'next/server';
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 
-export async function GET(req: NextRequest, { params }: { params: { kindeAuth: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { kindeAuth: string } }): Promise<Response> {
   const kindeAuth = params.kindeAuth;
-  console.log('kindeAuth,', kindeAuth);
-  return handleAuth(req, kindeAuth);
+
+  console.log('kindeAuth:The params going on here are:', kindeAuth);
+
 }
