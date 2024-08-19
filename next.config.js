c59899da655908e80b5578d9b29593638bfcd558
Shehryar <shehryar.bajwa@gmail.com>
2024-08-19 03:23:08 -0400

    updated kindeAuth

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/sign-in',
-        destination: '/api/auth/login',
+        destination: '/api/auth/kindeAuth',
         permanent: true,
       },
       {
