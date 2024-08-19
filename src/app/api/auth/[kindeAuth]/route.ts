f8d8edbb047c971170cda76f2999e40881992149
Shehryar <shehryar.bajwa@gmail.com>
2024-08-19 03:05:01 -0400

    updated kindeAuth for vercel testing

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -4,7 +4,7 @@ import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 export async function GET(req: NextRequest, { params }: { params: { kindeAuth: string } }): Promise<Response> {
   const kindeAuth = params.kindeAuth;
 
-  console.log('kindeAuth:The params going on here are:', kindeAuth);
+  console.log('kindeAuth:The params going on here are:', process.env.KINDE_AUTH_SECRET, process.env.KINDE_CLIENT_ID, process.env.KINDE_CLIENT_SECRET, process.env.KINDE_ISSUER_URL, process.env.KINDE_SITE_URL, process.env.KINDE_POST_LOGOUT_REDIRECT_URL, process.env.KINDE_POST_LOGIN_REDIRECT_URL);
 
   // Make sure to await handleAuth since it is likely a promise
   const result = await handleAuth(req, kindeAuth);
