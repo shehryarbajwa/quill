e93d0b0c4d78dfeed432f603a39c7f1e72746b6e
Shehryar <shehryar.bajwa@gmail.com>
2024-08-23 19:36:38 -0400

    updated auth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,11 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
-  { params }: any
-) {
+  { params }: { params: { kindeAuth: string } }
+): Promise<NextResponse> {
   const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
+  const authResponse = await handleAuth(request, endpoint)
+  return NextResponse.json(authResponse)
 }
